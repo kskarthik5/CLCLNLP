941ed8cc34537fdb08b202ee63e04e336abcc1ef
Karthik <kskarthik20025@gmail.com>
2021-10-11 17:55:14 +0530

    added index.htm

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-    res.sendFile("./index.html");
+    res.send("./index.html");
 })
 
 app.listen(process.env.PORT || port, () => {
