360ac5b59122fd72db99ec64cba0691080823847
kskarthik5 <kskarthik20025@gmail.com>
2021-11-01 13:34:57 +0530

    commitxyz

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 app.get("/", (req, res) => {
     const url = req.originalUrl;
-    res.sendFile('html/homepage.html',{root: __dirname });
+    res.sendFile('/html/homepage.html',{root: __dirname });
 })
 app.get("/login", (req, res) => {
     const url = req.originalUrl;
@@ -15,15 +15,11 @@ app.get("/login", (req, res) => {
 })
 app.get("/dashboard", (req, res) => {
     const url = req.originalUrl;
-    res.sendFile('html/dashboard.html',{root: __dirname });
+    res.sendFile('/html/dashboard.html',{root: __dirname });
 })
 
 app.listen(process.env.PORT || port, () => {
 	console.log("listening 8080...");
 });
 
-app.use('/html', express.static(path.join(__dirname, "html")));
-app.use('/css', express.static(path.join(__dirname, "css")));
-app.use('/js', express.static(path.join(__dirname, "js")));
-app.use('/img', express.static(path.join(__dirname, "img")));
-app.use('/style.css', express.static(path.join(__dirname, "style.css")))
+app.use('/', express.static(path.join(__dirname, "/")))
