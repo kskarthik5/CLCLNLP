4e068b41a7522d725e012b60bc59f02c58a10c9d
Vishal Kandu <kanduvishal07@gmail.com>
2021-11-17 13:56:09 +0530

    add gui

--- scripts/userinfo.js
+++ scripts/userinfo.js
@@ -13,6 +13,6 @@ async function getUserInfo() {
     const name=data.name;
     const sem=data.sem;
     console.log(typeof(data));
-    var banner="<a>User : "+name+ "\nCurrent Sem : "+sem+"</a>";
+    var banner="<a><br>User : "+name+ "<br>Current Sem : "+sem+"</a>";
     document.getElementById("profile-banner").innerHTML=banner;
 }
